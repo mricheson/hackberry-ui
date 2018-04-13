fef5f1b49321de08a5d5ac906082e9bd567f7b1e
Mitchell Richeson <richeson_mitchell@network.lilly.com>
2018-04-13 14:23:00 -0400

    rest api

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const TSHEETS_TOKEN = 'S.4__5bef0f3becb66d52b38539bea60456fa8a76b06a';
+const url = 'https://rest.tsheets.com/api/v1/timesheets?start_date=2018-04-13&end_date=2018-04-13&supplemental_data=yes';
+const obj = { json: true, headers: { 'Authorization': `Bearer S.4__5bef0f3becb66d52b38539bea60456fa8a76b06a` } };
+fetch(url, obj)
+  //.then(result => result.json())
+  //.then(r => console.log(r));
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+
+  }
+
   render() {
+
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
       </div>
     );
   }
