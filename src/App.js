e0ca5e457757a676fa22be5bf0f59ac55187f76a
Mitchell Richeson <mricheson@dminc.com>
2018-05-02 13:13:46 -0400

    page to pdf

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import DatePicker from 'react-date-picker'
 import Timesheet from './components/Timesheet';
 import { SyncLoader } from 'react-spinners';
+import html2pdf from 'html2pdf.js';
 
 class App extends Component {
   constructor(props) {
@@ -15,6 +16,18 @@ class App extends Component {
     };
 
     this.onDateChange = this.onDateChange.bind(this);
+    this.printDocument = this.printDocument.bind(this);
+  }
+
+  printDocument() {
+    const input = document.getElementById('divToPrint');
+    html2pdf(input, {
+      margin: [.25, .75, .75, .75],
+      filename: `attendance-${this.state.dateSelected.toISOString().split('T')[0]}_${new Date().toISOString()}.pdf`,
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { dpi: 192, letterRendering: true },
+      jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+    });
   }
 
   componentWillMount() {
@@ -31,26 +44,36 @@ class App extends Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="row justify-content-center">
-          <DatePicker className="m-3"
-            value={this.state.dateSelected}
-            onChange={this.onDateChange}
-            clearIcon={null} />
+      <div>
+        <div id="divToPrint" >
+          <div className="container">
+            <div className="d-flex justify-content-between align-self-center">
+              <div />
+              <DatePicker className="m-3"
+                value={this.state.dateSelected}
+                onChange={this.onDateChange}
+                clearIcon={null} />
+
+              <button type="button" className="btn btn-link" data-html2canvas-ignore onClick={this.printDocument} aria-label="Download as PDF">
+                <i className="fas fa-download"></i>
+              </button>
+
+            </div>
+            <div className="row">
+              <Timesheet loading={!this.state.attendanceReport} attendanceReport={this.state.attendanceReport} />
+            </div>
+            <div className="row justify-content-center m-5">
+              <SyncLoader
+                color={'#123abc'}
+                size={10}
+                height={10}
+                margin={'15px'}
+                loading={!this.state.attendanceReport}
+              />
+            </div>
+          </div>
         </div>
-        <div className="row">
-          <Timesheet loading={!this.state.attendanceReport} attendanceReport={this.state.attendanceReport} />
-        </div>
-        <div className="row justify-content-center m-5">
-          <SyncLoader
-            color={'#123abc'}
-            size={10}
-            height={10}
-            margin={'15px'}
-            loading={!this.state.attendanceReport}
-          />
-        </div>
-      </div>
+      </div >
     );
   }
 }
